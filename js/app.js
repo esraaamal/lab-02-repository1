29bef64ab2394a7615dc0c195978dd705ee91bcb
esraaamal <ma.esraa@hotmail.com>
2020-04-01 00:30:49 +0300

    edit

--- js/app.js
+++ js/app.js
@@ -41,22 +41,17 @@ function Product (filePath,title,keyWords,description,horns){
 }
 
 Product.prototype.render = function(){
-        // let subDiv = $('<div class="append1"></div>');
-        // let head1 =$("<h2></h2>").text(this.title);
-        // let elem =$("<li></li>").html(`<img src="${this.filePath}">`);
-        //  let paragraph=$("<p></p>").text(this.description);
-        // $('#showImage').append(subDiv);
-        //  $('.append1').append(head1);
-        // $('.append1').append(elem);
-        // $('.append1').append(paragraph);
-        // subDiv.removeClass('append1');
+        let subDiv = $('<div class="append1"></div>');
+        let head1 =$("<h2></h2>").text(this.title);
+        let elem =$("<li></li>").html(`<img src="${this.filePath}">`);
+         let paragraph=$("<p></p>").text(this.description);
+        $('#showImage').append(subDiv);
+         $('.append1').append(head1);
+        $('.append1').append(elem);
+        $('.append1').append(paragraph);
+        subDiv.removeClass('append1');
         $('img').height(300);
         $('img').width(300);
-        let template = $('#neigh-template').html();
-        //2- use the Mustache to render new html by merging the template with the data
-        // render(string,object)
-        let html = Mustache.render(template,this);
-        return html;
 
 }
 // console.log(keywordArr);
@@ -66,14 +61,20 @@ Product.prototype.renderOption = function(i){
     $('#filter').append(option);
 
 }
+// Product.prototype.selectRender = function(){
+//     $('#showImage div').hide();
+//     productArr.keyWords.forEach( value => {
 
+//     })
+//     if (vari === )
+// }
 
 Product.prototype.event1=function(){
     $("select").change(function(){
-let vari= $("select").val();//$(this).val its work too
+let vari= $("select").val();
 // console.log(vari);
 // var selet1 =productArr.map((n)=> n.keyWords);
-let select2 =productArr.filter((n) => n.keyWords === vari);
+var select2 =productArr.filter((n) => n.keyWords === vari);
 $('#showImage div').hide();
 select2.forEach(value => value.render() );
 console.log(select2);
@@ -83,28 +84,4 @@ $('#showImage div').show();
 
 }
     });
-}
-Product.prototype.event2=function(){
-    $('#sort').change(function(){
-      let choose= $('#sort').val();
-      console.log(choose);
-      if(choose === 'text'){
-        console.log('before ',productArr);
-        productArr.sort((a,b) => {
-          return a.title.toUpperCase() < b.title.toUpperCase();
-        });
-        console.log('after ',productArr);
-  
-        $('#showImage div').hide();
-        productArr.forEach(val => val.render());
-      }
-      else if (choose === 'num'){
-        productArr.sort((a,b) => {
-          return a.horns - b.horns;});
-        $('#showImage div').hide();
-        console.log(productArr);
-        productArr.forEach(val => val.render());
-      }
-    });
-
 }
